e2a4e1bf7af3992ab382b494815629f878d30c99
Elise <elise.beaussart@viacesi.fr>
2021-11-21 12:10:07 +0100

    Creation projet

---
+++ AppliConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppliConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
