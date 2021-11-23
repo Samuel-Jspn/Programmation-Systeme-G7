567dc58e4cfd8b4250b2a31016491208851ae818
sam.josephine@outlook.fr <sam.josephine@outlook.fr>
2021-11-23 13:48:15 +0100

    Begin Resource Manager on Program
    Mise en place des fichiers ressource pour les langues

--- AppliConsole/Program.cs
+++ AppliConsole/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Resources;
+using System.Reflection;
+using System.Threading;
+using System.Globalization;
 
 namespace AppliConsole
 {
@@ -6,7 +10,33 @@ namespace AppliConsole
     {
         static void Main(string[] args)
         {
-            Controller controller = new Controller();
+            ResourceManager rm = new ResourceManager("AppliConsole.Resources.Strings",
+                Assembly.GetExecutingAssembly());
+
+            Console.WriteLine("Veuillez choisir la langue.");
+            Console.WriteLine("1 : Français, 2 : English");
+            string value = Console.ReadLine();
+            if (value == "1")
+            {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("fr-FR");
+            Console.WriteLine(Thread.CurrentThread.CurrentUICulture.Name);
+            Console.WriteLine(rm.GetString("TestFR"));
+            Console.ReadLine();
+            }
+            if (value == "2")
+            {
+                Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("en-US");
+                Console.WriteLine(Thread.CurrentThread.CurrentUICulture.Name);
+                Console.WriteLine(rm.GetString("TestEN"));
+                Console.ReadLine();
+            }
+            //Console.WriteLine(Thread.CurrentThread.CurrentUICulture.Name);
+            
+            //Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("en-US");
+            //Console.WriteLine(Thread.CurrentThread.CurrentUICulture.Name);
+            //Console.WriteLine(rm.GetString("Welcome"));
+
+            //Controller controller = new Controller();
         }
     }
 }
